96ae7d1afcd404f9d9250a7eaeb45da15b973ff7
Joss Dixon <josscdixon@gmail.com>
2022-02-10 14:34:31 +0000

    validations added

--- app/models/aquatic.rb
+++ app/models/aquatic.rb
@@ -1,3 +1,12 @@
 class Aquatic < ApplicationRecord
   belongs_to :user
+
+  validates :purchased_on, presence: true
+  validates :nick_name, presence: true, length: { minimum: 2 }
+  validates :common_name, presence: true, length: { minimum: 2 }
+  validates :notes, presence: true, length: { maximum: 200 }
+  validates :scientific_name, presence: true, format: { with: /\A.*\s.*\z/, message: "Binomial nomenclature." }
+  validates :length, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+  validates :max_lifespan, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
 end
