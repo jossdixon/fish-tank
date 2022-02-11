77ec56ecd1d28cc8212825892a842f85935c5b12
Joss Dixon <josscdixon@gmail.com>
2022-02-11 12:08:33 +0000

    form testing incomplete

--- spec/views/aquatics/new.html.erb_spec.rb
+++ spec/views/aquatics/new.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "aquatics/new", type: :view do
-  current_user = User.first_or_create!(email: 'terence@mail.com', password: 'password', password_confirmation: 'password')
+  current_user = User.first_or_create!(email: 'me@mail.com', password: 'password', password_confirmation: 'password')
 
   before(:each) do
     assign(:aquatic, Aquatic.new(
@@ -9,7 +9,7 @@ RSpec.describe "aquatics/new", type: :view do
         common_name: "common name",
         scientific_name: "scientific name",
         sex: 'male',
-        purchased_on: '2022-02-02',
+        purchased_on: Date.new(2022, 02, 02),
         length: 10,
         max_lifespan: 5,
         user: current_user,
@@ -19,23 +19,14 @@ RSpec.describe "aquatics/new", type: :view do
 
   it "renders new aquatic form" do
     render
-
     assert_select "form[action=?][method=?]", aquatics_path, "post" do
-
       assert_select "input[name=?]", "aquatic[nick_name]"
-
       assert_select "input[name=?]", "aquatic[common_name]"
-
       assert_select "input[name=?]", "aquatic[scientific_name]"
-
-      assert_select "input[type=?][name=?]", "date", "aquatic[purchased_on]"
-
+      assert_select "input[name=?]", "aquatic[purchased_on]"
+      assert_select "input[name=?]", "aquatic[sex]"
       assert_select "input[name=?]", "aquatic[length]"
-
-      # assert_select "input[name=select]", "aquatic[sex]"
-
       assert_select "input[name=?]", "aquatic[max_lifespan]"
-
       assert_select "input[name=?]", "aquatic[notes]"
     end
   end
